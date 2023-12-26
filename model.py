327efa897296e5e7e48d26310f2c1b70ce83de6e
Eyal Arkin <eyalarkin@Eyals-MacBook-Pro.local>
2023-12-25 22:22:51 -0500

    first commit

---
+++ model.py
@@ -0,0 +1,11 @@
+import csv
+import sys
+
+def main(argv):
+    if (len(argv) != 4):
+        print("make sure to include three csv files for correct calculation")
+    else:
+        print("everything will hit")
+
+if __name__ == "__main__":
+    main(sys.argv)
